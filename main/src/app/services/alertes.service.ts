c48afebcbcbe6b664890185b1fc2db217152e845
yosr2003 <yosr.amamou@yahoo.com>
2025-06-05 14:28:18 +0100

    .................

---
+++ main/src/app/services/alertes.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TokenStorageService } from './token-storage-service.service';
+import { map, Observable } from 'rxjs';
+import { Transfert } from '../classes/transfert';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertesService {
+
+ constructor(private http: HttpClient) {}
+
+getTransfertsEnAttente(): Observable<Transfert[]> {
+  return this.http.get<Transfert[]>('http://localhost:8085/transferts/alerte');
+}
+
+// getAlertesEcheances(): Observable<AmortissementEcheance[]> {
+//   return this.http.get<any>('http://localhost:8085/alertsUpcomingDeadline').pipe(
+//     map(response => response.body.data as AmortissementEcheance[])
+//   );
+// }
+
+
+}
